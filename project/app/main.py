e99066a41d508b0f36e58ab05017b3ae7da9ac2b
aejunior <user.emerson@outlook.com>
2022-11-21 22:04:52 -0300

    Adicionando entidades do ERM atualizado

--- project/app/main.py
+++ project/app/main.py
@@ -1,9 +1,9 @@
 from fastapi import Depends, FastAPI
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from fastapi.responses import JSONResponse
 from app.db import get_session
-from app.models import Song, SongCreate
+
 
 app = FastAPI()
 
@@ -13,17 +13,17 @@ async def pong():
     return {"ping": "pong!"}
 
 
-@app.get("/songs", response_model=list[Song])
-async def get_songs(session: AsyncSession = Depends(get_session)):
-    result = await session.execute(select(Song))
-    songs = result.scalars().all()
-    return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id) for song in songs]
+# @app.get("/songs", response_model=list[Song])
+# async def get_songs(session: AsyncSession = Depends(get_session)):
+#     result = await session.execute(select(Song))
+#     songs = result.scalars().all()
+#     return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id) for song in songs]
 
 
-@app.post("/songs")
-async def add_song(song: SongCreate, session: AsyncSession = Depends(get_session)):
-    song = Song(name=song.name, artist=song.artist, year=song.year)
-    session.add(song)
-    await session.commit()
-    await session.refresh(song)
-    return song
+# @app.post("/songs")
+# async def add_song(song: SongCreate, session: AsyncSession = Depends(get_session)):
+#     song = Song(name=song.name, artist=song.artist, year=song.year)
+#     session.add(song)
+#     await session.commit()
+#     await session.refresh(song)
+#     return song
--- project/app/models.py
+++ project/app/models.py
@@ -1,17 +1,56 @@
 from typing import Optional
+from datetime import datetime
+from sqlmodel import SQLModel, Field, Column, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import UUID
 
-from sqlmodel import SQLModel, Field
-
-
-class SongBase(SQLModel):
-    name: str
-    artist: str
-    year: Optional[int] = None
-
-
-class Song(SongBase, table=True):
+import uuid
+class User(SQLModel, table=True):
+    """
+        TiposDeUsuarios:
+        id
+        login
+        senha
+        cpf
+        tipos_de_usuarios
+        nome
+    """
     id: int = Field(default=None, primary_key=True)
+    username: str 
+    password: str
+    cpf: int
+    perms: int
+    name: str
+
+class Ocorrencia(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
+    address: str
+    telefone: Optional[str]
+    created_at: Optional[datetime] = Field(
+        sa_column=Column(DateTime(timezone=True), server_default=func.now())
+    )
+    cidadao_id: int
+    agente_id: int
+    uuid_ocorrencia = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    alerta_id: int
+    instituicao_id: int
+    long: Optional[str]
+    lati: Optional[str]
 
 
-class SongCreate(SongBase):
-    pass
+class tipoOcorrenciaAutuacao(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    nome: str
+    descricao: str
+    prioridade: int
+    tipo: str
+    instituicao_id: int
+
+
+class instituicaoCompetente(SQLModel, table=True):
+    id: Field(default=None, primary_key=True)
+    nome: str
+    endereco: str
+    telefone: int
+    email: str
